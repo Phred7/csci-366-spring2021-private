d93e29168a7c1e51015f623fc1a5ce076284bb78
Phred7 <woward00@gmail.com>
2021-04-25 00:20:34 -0700

    Networking work

--- battlebit/src/server.c
+++ battlebit/src/server.c
@@ -97,6 +97,82 @@ int handle_client_connect(int player) {
 
 }
 
+/*int handle_client_connect(int player) {
+    // STEP 8 - This is the big one: you will need to re-implement the REPL code from
+    // the repl.c file, but with a twist: you need to make sure that a player only
+    // fires when the game is initialized and it is there turn.  They can broadcast
+    // a message whenever, but they can't just shoot unless it is their turn.
+    //
+    // The commands will obviously not take a player argument, as with the system level
+    // REPL, but they will be similar: load, fire, etc.
+    //
+    // You must broadcast informative messages after each shot (HIT! or MISS!) and let
+    // the player print out their current board state at any time.
+    //
+    // This function will end up looking a lot like repl_execute_command, except you will
+    // be working against network sockets rather than standard out, and you will need
+    // to coordinate turns via the game::status field.
+    int client_socket = SERVER->player_sockets[player];
+    struct game * game_curr = game_get_current();
+    int read_size;
+    int buffer_size = 2000;
+    char* raw_buffer = NULL;
+    struct char_buff *output = cb_create(buffer_size);
+    char message[100] = {0};
+
+    sprintf(message,"Welcome to battleBit server Player %d\n", player);
+    send(client_socket, message, strlen(message), 0);
+    cb_reset(output); //resets to ""
+    cb_append(output, "battleBit (? for help) > ");
+    cb_write(client_socket, output);
+
+    while ((read_size = recv(client_socket, raw_buffer, buffer_size, 0)) > 0) {
+        //take raw_buffer and dump into input char bf (tokenize?)
+        //mimic repl_execture_cmd()... pass through the current player (so that client cannot fire for the other player)... ignore cb_free()
+        //big ugly if statements
+
+        pthread_mutex_lock(&lock); //mutex lock
+        cb_reset(output);
+        struct char_buff *input = cb_create(2000);
+        cb_append(&input, &raw_buffer);
+        char* command = cb_tokenize(&input, " \n");
+        /*if (command) {
+            char* arg1 = cb_next_token(&raw_buffer);
+            char* arg2 = cb_next_token(&raw_buffer);
+            if (strcmp(command, "exit") == 0) {
+                cb_append(output, "\ngoodbye, you scrub!");
+                cb_write(client_socket, output);
+                exit(EXIT_SUCCESS);
+            } else if (strcmp(command, "?") == 0) {
+                cb_append(output,"? - show help\n");
+                cb_append(output,"load [0-1] <string> - load a ship layout file for the given player\n");
+                cb_append(output,"show [0-1] - shows the board for the given player\n");
+                cb_append(output,"fire [0-1] [0-7] [0-7] - fire at the given position\n");
+                cb_append(output,"say <string> - Send the string to all players as part of a chat\n");
+                cb_append(output,"reset - reset the game\n");
+                cb_append(output,"server - start the server\n");
+                cb_append(output,"exit - quit the server\n");
+            } else {
+                char message[100] = {0};
+                sprintf(message,"Unknown Command: %s\n", command);
+                cb_append(output, message);
+            }
+
+        }*/
+        char message[100] = {0};
+        sprintf(message,"Unknown Command: %s\n", command);
+        cb_append(output, message);
+
+        cb_append(output, "\nbattleBit (? for help) > ");
+        cb_write(client_socket, output);
+    }
+    pthread_mutex_unlock(&lock); //mutex_unlock
+
+    //use mutexes: (declared outside of this func) pthread_mutex_lock and pthread_mutex_unlock around anywhere data is being manipulated
+    //(so only one client can change data at a time)... just inside of this func works... dont need fine grain locking
+    //protect data in game.c... 2 threads cannot be even looking at the data at the same time
+}*/
+
 void server_broadcast(char_buff *msg) {
 
     // send message to all players
